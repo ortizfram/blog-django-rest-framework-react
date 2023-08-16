923c5542cfa5a3e5aa15ab1a6afc190a4c4e2197
ortizfram <ortizfranco48@gmail.com>
2023-08-16 12:34:26 -0300

    app routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 
+import Error404 from "containers/errors/Error404";
+import Home from "containers/pages/Home";
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Provider>
+      <Router>
+        <Routes>
+          {/* Error Display */}
+          <Route path="*" element={<Error404/>}/>
+
+          {/* Home Display */}
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/containers/pages/Home.jsx
@@ -0,0 +1,21 @@
+import FullWithLayout from 'hocs/layouts/FullWithLayout'
+import {connect} from 'react-redux'
+import React from 'react'
+
+function Home() {
+  return (
+    <FullWithLayout>
+        <div>
+            Home
+        </div>
+    </FullWithLayout>
+  )
+}
+
+const mapStateToProps = state =>({
+
+})
+
+export default connect(mapStateToProps,{
+
+})(Home)
