2f9293dd34cd58b268255af22e5bef806633d836
ortizfram <ortizfranco48@gmail.com>
2023-08-16 10:54:49 -0300

    blog view post detail view

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('', BlogListView.as_view()),
+    path('<post_slug>/', PostDetailView.as_view()),
+]
