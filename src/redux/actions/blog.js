f11d3238128860f91b53b1edee05ad973c2fc469
ortizfram <ortizfranco48@gmail.com>
2023-08-16 13:00:00 -0300

    redux actions get_blog_list_page

--- src/redux/actions/blog.js
+++ src/redux/actions/blog.js
@@ -36,3 +36,33 @@ export const get_blog_list = () => async dispatch => {
         });
     }
 }
+
+export const get_blog_list_page = (p) => async dispatch => {
+
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+
+    try{
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/?p=${p}`, config);
+
+        if (res.status === 200) {
+            dispatch({
+                type: GET_BLOG_LIST_SUCCESS,
+                payload: res.data
+            });
+        } else {
+            dispatch({
+                type: GET_BLOG_LIST_FAIL
+            });
+        }
+
+    }catch{
+        dispatch({
+            type: GET_BLOG_LIST_FAIL
+        });
+    }
+}
